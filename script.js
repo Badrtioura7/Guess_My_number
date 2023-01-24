d4b202fa69560b155694c14ddddc5d8bef2e7f74
Badr Tioura <@mohamedtioura7@gmail.com>
2023-01-24 13:05:50 -0500

    finishing Styles

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// 
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,6 +16,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+    document.querySelector('.number').textContent = secretNumber;
+    if(score>=highScore){
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   else if(guess > secretNumber){
     document.querySelector('.message').textContent = 'Too High!';
@@ -33,3 +41,24 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost!';}
 }
 }});
+document.querySelector('.again').addEventListener('click',function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing ...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '30rem'
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+
+  
+  
+  
+  
+  
+  }
+  
+  
+  
+  
+  );
