62e7af8378912b7a91c6a08fd79a569e3a611763
Badr Tioura <@mohamedtioura7@gmail.com>
2023-01-24 12:12:04 -0500

    setting up game logic

--- script.js
+++ script.js
@@ -3,12 +3,33 @@
 // (document.querySelector('.message').textContent) = 'correct Number';
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
-document.querySelector('.check').addEventListener('click', function () {
-const guess = Number(document.querySelector('.guess').value);
-console.log( guess , typeof guess);
-if(!guess){
-  document.querySelector('.message').textContent = 'No number';
-}
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
-});
-git push origin main 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if(score>0){
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  }
+  else if(guess > secretNumber){
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  
+    if (score === 0 ) {
+      document.querySelector('.message').textContent = 'You lost!';}
+  }
+  else if(guess < secretNumber){
+    document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  
+    if (score === 0 ) {
+      document.querySelector('.message').textContent = 'You lost!';}
+}
+}});
